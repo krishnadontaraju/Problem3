bac103c1dcc474a3a6834eeb38c753220f82d66c
Sai Krishna <reachkrishnadontaraju@gmail.com>
2021-03-09 22:13:20 +0530

    [SAI] created the singlet code

--- flip_coin_combination.sh
+++ flip_coin_combination.sh
@@ -1,8 +1,23 @@
 #!/bin/bash
 
-if [ $(( RANDOM % 2 )) -eq 0 ]
+declare -a singlet_array
+declare -A singlet_dictionary=( [1]="Heads" [2]="Tails" )
+for (( i=0 ;i<20 ;i++ ))
+do
+        singlet_substitute=$(( RANDOM % 2 + 1 ))
+        singlet_array[$singlet_substitute]=$(( singlet_array[$singlet_substitute] + 1 ))
+done
+
+for single in ${!singlet_array[@]}
+do
+        echo "${singlet_dictionary[$single]} -- ${singlet_array[$single]} times"
+done
+
+#head_count=${singlet_array[1]}
+#tail_count=${singlet_array[2]}
+if [ ${singlet_array[1]} -gt ${singlet_array[2]} ]
 then
-	echo "HEADS"
+	echo "HEADS WON BY $(( ( ${singlet_array[1]} * 100 ) / ( ${singlet_array[2]} + ${singlet_array[1]} ) )) %"
 else
-	echo "TAILS"
+	echo "TAILS WON BY $(( ( ${singlet_array[2]} * 100 ) / ( ${singlet_array[1]} + ${singlet_array[1]} ) )) %"
 fi
